29f785e0684ffebe7a6db00dd12e61faec2ee872
Soham Chowdhury <chow.soham@gmail.com>
2016-12-23 20:02:06 +0530

    Fix broken links in footer

--- refinator/views/static_pages.py
+++ refinator/views/static_pages.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/about.djhtml')
 
 
 def contact(request):
-    return render(request, 'main/contact.html')
+    return render(request, 'main/contact.djhtml')
